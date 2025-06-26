ca6a86e6f7c4135719f5bd19f996c495d30a3143
macbright <macbrightabong@gmail.com>
2025-06-26 11:30:52 +0100

    Set ALLOWED_HOSTS for PythonAnywhere deployment

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['macbright19.pythonanywhere.com']
 
 
 # Application definition
